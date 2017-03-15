d9d6e1e28f0af55b4fe312bc4f6c6331da9b7652
themartinsjoseph <themartinsjoseph@gmail.com>
2017-03-15 08:43:16 -0700

    fixed a minor HTML error

--- js/app.js
+++ js/app.js
@@ -8,40 +8,40 @@ app.controller('ListCtrl', ['$scope', function($scope){
 
 	$scope.addFruitItem = function(){
 			$scope.fruitsList.push($scope.poolList.splice(this.$index, 1)[0]);
-			$scope.item = '';
+			// $scope.item = '';
 	};
 
 	$scope.addVegItem = function(){
 		$scope.vegList.push($scope.poolList.splice(this.$index, 1)[0]);
-		$scope.item = '';
+		// $scope.item = '';
 	};
 
 	$scope.addFruitToPool = function() {
 		$scope.poolList.push($scope.fruitsList.splice(this.$index, 1)[0]);
-		$scope.item = '';
+		// $scope.item = '';
 	};
 
 	$scope.addVegToPool = function() {
 		$scope.poolList.push($scope.vegList.splice(this.$index, 1)[0]);
-		$scope.item = '';
+		// $scope.item = '';
 	};
 
 	//How to know if the item is a fruit or vegetable 
 	//The includes() method determines whether an array includes a certain element, returning true or false as appropriate.
-	$scope.inFruits = function() {
-	if (fruits.includes(this.$index, 1)[0]) {
-		return true; 
-	} else {
-		return false; 
-	}
-	};
+	// $scope.inFruits = function() {
+	// if (fruits.includes(this.$index, 1)[0]) {
+	// 	return true; 
+	// } else {
+	// 	return false; 
+	// }
+	// };
 
-	$scope.inVegetables = function() {
-		if (vegetables.includes(this.$index, 1)[0]) {
-			return true; 
-		} else {
-			return false 
-		}
-	};
+	// $scope.inVegetables = function() {
+	// 	if (vegetables.includes(this.$index, 1)[0]) {
+	// 		return true; 
+	// 	} else {
+	// 		return false 
+	// 	}
+	// };
 
 }]);
